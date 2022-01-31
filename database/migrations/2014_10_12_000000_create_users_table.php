d45f29c756243ace7c843b587f30b969dce54ec3
lanre <larylivlynnxe@gmail.com>
2022-01-31 17:13:38 +0100

    feat push

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,8 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('otp')->nullable();
+            $table->date('otp_expire')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
